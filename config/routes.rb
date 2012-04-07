b0b47fff956cd2233d051591e2a4d1fba71ce4ca
romaimperator <romaimperator@gmail.com>
2012-04-07 13:43:32 -0500

    Created User controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 RailsDemo::Application.routes.draw do
   devise_for :users
 
+  resources :users
+
   root :to => "home#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
