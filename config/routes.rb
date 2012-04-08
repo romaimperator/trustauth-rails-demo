db026321a02d48e048848dcd45c82fa3f483aac9
romaimperator <romaimperator@gmail.com>
2012-04-07 22:02:32 -0500

    Removed access to the users controller.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ RailsDemo::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => 'users/omniauth_callbacks' }
 
   resources :notes
-  resources :users
 
   root :to => "home#index"
   # The priority is based upon order of creation:
