916d68a44646dfbc5f932be8e42220579d32b483
romaimperator <romaimperator@gmail.com>
2012-04-07 21:53:58 -0500

    After login redirect to requested page or default to notes index.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  def after_sign_in_path_for(resource)
+    return request.env['omniauth.origin'] || stored_location_for(resource) || notes_path
+  end
 end
