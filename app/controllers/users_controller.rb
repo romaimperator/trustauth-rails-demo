1053961131c4fe1d6e066f302501853696259bd9
romaimperator <romaimperator@gmail.com>
2012-04-09 21:50:05 -0500

    Added back in the new and create actions for the users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,27 @@
 class UsersController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :except => [:new, :create]
+
+  def new
+    @user = User.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @user }
+    end
+  end
+
+  def create
+    params[:user][:public_key] = session[:public_key] if session[:public_key]
+    params[:user][:password] ||= SecureRandom.hex(30)
+    @user = User.new params[:user]
+
+    if @user.save!
+      sign_in_and_redirect(:user, @user)
+    else
+      respond_to do |format|
+        format.html { render action: "new" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end
