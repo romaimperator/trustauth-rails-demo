5e79ab09581637df62f74b9bc302efb0c4b4c3d8
romaimperator <romaimperator@gmail.com>
2012-04-09 21:49:17 -0500

    Changed OmniAuth handler to redirect to create enter the email.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,13 +1,20 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def trustauth
     if auth_hash['info']['result']
-      user = User.find_or_create_by_public_key(auth_hash['info']['public_key'])
+      user = User.find_by_public_key(auth_hash['info']['public_key'])
 
-      if not user_signed_in?
-        sign_in_and_redirect(:user, user) and return
+      if not user
+        # This user doesn't have an account so save the public key and redirect them to the page to enter an email.
+        session[:public_key] = auth_hash['info']['public_key']
+        redirect_to new_user_path and return
+      else
+        if not user_signed_in?
+          sign_in_and_redirect(:user, user) and return
+        end
       end
+    else
+      redirect_to root_path, :notice => "Authentication failed."
     end
-    redirect_to root_path
   end
 
   def auth_hash
