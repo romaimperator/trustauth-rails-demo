4fa80d2dc526cffc40c37d89f6be6ced09b3f335
Daniel Fox <romaimperator@gmail.com>
2014-04-20 19:48:13 -0500

    Adding task to rollback a release

--- config/deploy.rb
+++ config/deploy.rb
@@ -112,5 +112,22 @@ namespace "deploy" do
     queue "test -s \"#{pid}\" && kill -USR2 `cat #{pid}`"
     #queue 'touch tmp/restart.txt'
   end
+
+  desc "Rolls back the latest release"
+  task :rollback => :environment do
+    queue! %[echo "-----> Rolling back to previous release for instance: #{domain}"]
+
+    # Delete existing sym link and create a new symlink pointing to the previous release
+    queue %[echo -n "-----> Creating new symlink from the previous release: "]
+    queue %[ls "#{deploy_to}/releases" -Art | sort | tail -n 2 | head -n 1]
+    queue! %[ls -Art "#{deploy_to}/releases" | sort | tail -n 2 | head -n 1 | xargs -I active ln -nfs "#{deploy_to}/releases/active" "#{deploy_to}/current"]
+
+    # Remove latest release folder (active release)
+    queue %[echo -n "-----> Deleting active release: "]
+    queue %[ls "#{deploy_to}/releases" -Art | sort | tail -n 1]
+    queue! %[ls "#{deploy_to}/releases" -Art | sort | tail -n 1 | xargs -I active rm -rf "#{deploy_to}/releases/active"]
+
+    invoke :'deploy:restart'
+  end
 end
 
