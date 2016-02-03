5d15845ddba4786292991a44d50192ad4d688c72
Daniel Fox <romaimperator@gmail.com>
2016-02-03 16:59:53 -0600

    updating devise config

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => 'users/omniauth_callbacks', :registrations => 'users' }
+  #devise_for :users, :controllers => { :registrations => 'users' }
 
   resources :notes
   resources :users
 
   root :to => "static#index"
 
-  match ':action' => 'static#:action'
+  get ':action' => 'static#:action'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
